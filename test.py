865813a0c9da8070d0b0aa37d6c08fd5d5146a36
Mohammad4lx <60289777+Mohammad4lx@users.noreply.github.com>
2023-08-03 16:05:09 +0330

    Createtest.py

---
+++ test.py
@@ -0,0 +1,84 @@
+#کتابخانه های پردازش زبان طبیعی
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+from nltk.tokenize import word_tokenize
+
+
+#کتابخانه های ریاضی و محاسباتی
+import numpy as np
+
+#کتابخانه های یادگیری ماشین و شبکه عصبی
+import tensorflow as tf
+import tensorflow_datasets as tfds
+from tensorflow import keras
+
+#کتابخانه های یادگیری ماشین
+from sklearn.preprocessing import LabelEncoder
+
+#کتابخانه پردازش صدا  
+import speech_recognition as sr
+
+
+#کتابخانه موتور پردازش زبان طبیعی
+import spacy
+
+# کتابخانه تبدیل گفتار به متن
+import pyaudio
+
+# Imports
+from transformers import AutoModel, AutoTokenizer
+
+
+
+
+# Load pre-trained model & tokenizer
+model = AutoModel.from_pretrained("HooshvareLab/bert-fa-base-uncased")
+tokenizer = AutoTokenizer.from_pretrained("HooshvareLab/bert-fa-base-uncased")
+
+# Sample text 
+text = "این یک متن نمونه برای تست مدل ParsBERT است"
+
+# Tokenize  
+inputs = tokenizer(text, return_tensors="pt")
+
+# Forward pass  
+outputs = model(**inputs)
+
+# Get embeddings
+embeddings = outputs.last_hidden_state
+print(embeddings.shape)
+
+# You can now use embeddings for downstream tasks
+
+
+# Class for speech recognition
+class SpeechRecognizer:
+    
+    def __init__(self):
+        self.recognizer = sr.Recognizer()
+
+    def recognize_speech(self, audio):
+        text = self.recognizer.recognize_google(audio, language="fa-IR")
+        return text
+
+# Class for NLP  
+class NLPProcessor:
+
+    def __init__(self):
+        self.stop_words = set(stopwords.words('farsi')) 
+
+    def extract_features(self, text):
+        tokens = word_tokenize(text)
+        tokens = [tok for tok in tokens if tok not in self.stop_words]
+        return tokens
+
+# Bringing it together 
+recognizer = SpeechRecognizer()
+audio = recognizer.listen() 
+text = recognizer.recognize_speech(audio)
+
+processor = NLPProcessor()
+features = processor.extract_features(text)
+
+print(features)
